d9433b2f1f659aaa7982f0a373f674823379077f
Lucas Dahl <LucasDahl425@yahoo.com>
2019-01-22 06:42:26 -0800

    Added placeholder text.

--- Sharring/ViewController.swift
+++ Sharring/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        textField.placeholder = "Type something to save..."
     }
 
 
